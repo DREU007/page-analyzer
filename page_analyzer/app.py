70845568553a5ae9e103715e7dea1ab0e5843d51
DREU007 <dreu007@gmail.com>
2023-08-22 22:22:55 +0400

    Add db_processor.py; Update database.sql app.py locales.json poetry.lock todo.txt

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,13 +1,15 @@
 import os
-import datetime
-import psycopg2
-import psycopg2.extras
 from flask import (
         Flask, render_template, redirect, url_for, make_response, request, flash,
         get_flashed_messages
 )
+import psycopg2
+import psycopg2.extras
+
 from page_analyzer.locales_loader import Locales
 from page_analyzer.url_tools import normalize, validate
+from page_analyzer.db_processor import DB
+
 from dotenv import load_dotenv
 
 
@@ -20,6 +22,7 @@ DATABASE_URL = os.getenv('DATABASE_URL')
 conn = psycopg2.connect(
         DATABASE_URL, cursor_factory=psycopg2.extras.RealDictCursor
 )
+db = DB(conn)
 locales = Locales()
 
 
@@ -55,32 +58,8 @@ def not_found(e):
 
 @app.route('/urls', methods=['GET'])
 def get_urls():
-    with conn.cursor() as curr:
-        curr.execute("SELECT * FROM urls ORDER BY id DESC;")
-        sql_data = curr.fetchall()
+    sql_data = db.get_urls_data()
     return render_template('urls.html', table_data=sql_data) 
-
-def get_existing_urls():
-    with conn.cursor() as curr:
-        curr.execute('SELECT name FROM urls;')
-        sql_data = curr.fetchall()
-    if sql_data:
-        return [row['name'] for row in sql_data]
-    return []
-
-def insert_url(normalized_url):
-    with conn.cursor() as curr:
-        curr.execute(
-            'INSERT INTO urls (name, created_at) VALUES (%s, %s);',
-            (normalized_url, datetime.datetime.now().isoformat())
-        )
-        conn.commit()
-
-def get_url_id_by_name(normalized_url):
-    with conn.cursor() as curr:
-        curr.execute('SELECT id FROM urls WHERE name = %s', (normalized_url,))
-        url_id = curr.fetchone()['id']
-    return url_id
 
 @app.route('/urls', methods=['POST'])
 def post_urls():
@@ -95,22 +74,31 @@ def post_urls():
         flash('invalid', 'danger') 
         return make_response(redirect(url_for('get_index'), code=302))
 
-    existing_urls = get_existing_urls()
+    existing_urls = db.get_existing_urls()
     if normalized_url in existing_urls:
         flash('exist', 'info')
     else:
-        insert_url(normalized_url)
+        db.insert_url(normalized_url)
         flash('added', 'success')
 
-    url_id = get_url_id_by_name(normalized_url)
+    url_id = db.get_url_id_by_name(normalized_url)
     return make_response(redirect(
         url_for('get_url_id', url_id=url_id), code=302
     ))
-    
+
 @app.route('/urls/<int:url_id>')
 def get_url_id(url_id):
     messages = get_flashed_messages(with_categories=True)
-    with conn.cursor() as curr:
-        curr.execute("SELECT * FROM urls WHERE id = %s", (url_id,))
-        url_data = curr.fetchone()
-    return render_template('url_id.html', url_data=url_data, messages=messages)
+    url_data = db.get_url_data(url_id)
+    url_checks = db.get_checks_data(url_id)
+    return render_template(
+            'url_id.html',
+            url_data=url_data,
+            url_checks=url_checks,
+            messages=messages
+    )
+
+@app.route('/urls/<int:url_id>/checks', methods=['POST'])
+def post_url_id_checks(url_id):
+    db.insert_check(url_id) 
+    return redirect(url_for('get_url_id', url_id=url_id), 302)
---
+++ page_analyzer/db_processor.py
@@ -0,0 +1,59 @@
+import datetime
+
+
+class DB:
+    def __init__(self, conn):
+        self.conn = conn
+
+    def get_urls_data(self):
+        with self.conn.cursor() as curr:
+            curr.execute("""
+            SELECT DISTINCT ON (urls.id) urls.id,
+                urls.name, url_checks.status_code, url_checks.created_at
+                FROM urls LEFT JOIN url_checks ON urls.id = url_checks.url_id
+            ORDER BY urls.id DESC;
+            """)
+            sql_data = curr.fetchall()
+        return sql_data
+    
+    def get_existing_urls(self):
+        with self.conn.cursor() as curr:
+            curr.execute('SELECT name FROM urls;')
+            sql_data = curr.fetchall()
+        if sql_data:
+            return [row['name'] for row in sql_data]
+        return []
+    
+    def insert_url(self, normalized_url):
+        with self.conn.cursor() as curr:
+            curr.execute(
+                'INSERT INTO urls (name, created_at) VALUES (%s, %s);',
+                (normalized_url, datetime.date.today().isoformat())
+            )
+            self.conn.commit()
+    
+    def get_url_id_by_name(self, normalized_url):
+        with self.conn.cursor() as curr:
+            curr.execute('SELECT id FROM urls WHERE name = %s', (normalized_url,))
+            url_id = curr.fetchone()['id']
+        return url_id
+    
+    def insert_check(self, url_id):
+        with self.conn.cursor() as curr:
+            curr.execute(
+                "INSERT INTO url_checks (url_id, created_at) VALUES (%s, %s);",
+                (url_id, datetime.date.today().isoformat())
+            ) 
+            self.conn.commit()
+    
+    def get_checks_data(self, url_id):
+        with self.conn.cursor() as curr:
+            curr.execute("SELECT * FROM url_checks WHERE url_id = %s ORDER BY id DESC", (url_id,))
+            sql_data = curr.fetchall()
+            return sql_data
+    
+    def get_url_data(self, url_id):
+        with self.conn.cursor() as curr:
+            curr.execute("SELECT * FROM urls WHERE id = %s", (url_id,))
+            url_data = curr.fetchone()
+        return url_data
